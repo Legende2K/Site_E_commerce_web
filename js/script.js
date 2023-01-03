400627640845c31bd650f520865c5ca8fa946519
Zilba26 <basile.lebaudy@gmail.com>
2023-01-03 22:44:36 +0100

    SearchBar finish

--- js/script.js
+++ js/script.js
@@ -5,43 +5,20 @@ let suggestions = [
     "Assiettes",
 ];
 
-// getting all required elements
 const searchInput = document.querySelector("#search_bar");
 const input = searchInput.querySelector("input");
 const resultBox = searchInput.querySelector(".resultBox");
 
-// if user press any key and release
 input.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
+    let userData = e.target.value;
     let emptyArray = [];
     if(userData){
         emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
         });
         emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
             return data = '<li>&emsp;'+ data +'</li>';
         });
-        searchInput.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = resultBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }else{
-        searchInput.classList.remove("active"); //hide autocomplete box
     }
-}
-
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
-    }else{
-        listData = list.join('');
-    }
-    resultBox.innerHTML = listData;
+    resultBox.innerHTML = emptyArray.join('');
 }
