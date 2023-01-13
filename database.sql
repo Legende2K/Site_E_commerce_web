94c91f5ae38f96695d51bf82d88155db0a5a803a
Legende2K <106341506+Legende2K@users.noreply.github.com>
2023-01-14 00:46:32 +0100

    ajout fichier database.sql

---
+++ database.sql
@@ -0,0 +1,393 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 13, 2023 at 11:44 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `badge`
+--
+
+CREATE TABLE `badge` (
+  `BadgeID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `badge`
+--
+
+INSERT INTO `badge` (`BadgeID`, `Name`) VALUES
+(1, '-10%'),
+(2, '-20%'),
+(3, '-50%'),
+(4, '-40%'),
+(5, '-50%'),
+(6, '-60%'),
+(7, '-70%'),
+(8, 'Garantie 3 mois'),
+(9, 'Livraison Gratuite'),
+(10, 'Meilleure Vente');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `CartID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `ItemID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `CategoryID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`CategoryID`, `Name`) VALUES
+(1, 'Ameublement'),
+(2, 'Décoration'),
+(3, 'Vaiselle'),
+(4, 'Rangements'),
+(5, 'Casseroles, plats et poêles'),
+(6, 'Electroménager'),
+(7, 'Couteaux et ustensiles de cuisine'),
+(8, 'Accessoires de nettoyage'),
+(9, 'Café, thé et expresso');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expedition`
+--
+
+CREATE TABLE `expedition` (
+  `ExpeditionID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `expedition`
+--
+
+INSERT INTO `expedition` (`ExpeditionID`, `Name`) VALUES
+(1, 'La Poste'),
+(2, 'Chronopost'),
+(3, 'UPS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `ItemID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `SubCategoryID` int(11) NOT NULL,
+  `MaterialID` int(11) NOT NULL,
+  `PictureID` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `Parution` date NOT NULL,
+  `Resume` varchar(500) NOT NULL,
+  `BuyerID` int(11) NOT NULL,
+  `BadgeID` int(11) NOT NULL,
+  `StarRate` int(11) NOT NULL,
+  `SellerID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material`
+--
+
+CREATE TABLE `material` (
+  `MaterialID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `material`
+--
+
+INSERT INTO `material` (`MaterialID`, `Name`) VALUES
+(1, 'Verre'),
+(2, 'Inox'),
+(3, 'Céramique'),
+(4, 'Silicone'),
+(5, 'Bois'),
+(6, 'Téflon');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `picture`
+--
+
+CREATE TABLE `picture` (
+  `PictureID` int(11) NOT NULL,
+  `Picture` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `RankID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `AdminAccess` tinyint(1) NOT NULL,
+  `SellingAccess` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`RankID`, `Name`, `AdminAccess`, `SellingAccess`) VALUES
+(1, 'Acheteur', 0, 0),
+(2, 'Vendeur', 0, 1),
+(3, 'Administrateur', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `StockID` int(11) NOT NULL,
+  `ItemID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `SubCategoryID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `CategoryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Address1` varchar(100) NOT NULL,
+  `Address2` varchar(100) NOT NULL,
+  `PostalCode` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `Phone` int(11) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `ConfirmEmail` tinyint(1) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `RankID` int(11) NOT NULL,
+  `ExpeditionID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `badge`
+--
+ALTER TABLE `badge`
+  ADD PRIMARY KEY (`BadgeID`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`CartID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `ItemID` (`ItemID`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `expedition`
+--
+ALTER TABLE `expedition`
+  ADD PRIMARY KEY (`ExpeditionID`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`ItemID`),
+  ADD KEY `BadgeID` (`BadgeID`),
+  ADD KEY `BuyerID` (`BuyerID`),
+  ADD KEY `MaterialID` (`MaterialID`),
+  ADD KEY `SellerID` (`SellerID`),
+  ADD KEY `SubCategoryID` (`SubCategoryID`),
+  ADD KEY `PictureID` (`PictureID`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`MaterialID`);
+
+--
+-- Indexes for table `picture`
+--
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`PictureID`);
+
+--
+-- Indexes for table `rank`
+--
+ALTER TABLE `rank`
+  ADD PRIMARY KEY (`RankID`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`StockID`),
+  ADD KEY `ItemID` (`ItemID`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`SubCategoryID`),
+  ADD KEY `CategoryID` (`CategoryID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `ExpeditionID` (`ExpeditionID`),
+  ADD KEY `RankID` (`RankID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `ItemID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `picture`
+--
+ALTER TABLE `picture`
+  MODIFY `PictureID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `StockID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`ItemID`) REFERENCES `item` (`ItemID`);
+
+--
+-- Constraints for table `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`BadgeID`) REFERENCES `badge` (`BadgeID`),
+  ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`BuyerID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `item_ibfk_3` FOREIGN KEY (`MaterialID`) REFERENCES `material` (`MaterialID`),
+  ADD CONSTRAINT `item_ibfk_4` FOREIGN KEY (`SellerID`) REFERENCES `user` (`UserID`),
+  ADD CONSTRAINT `item_ibfk_5` FOREIGN KEY (`SubCategoryID`) REFERENCES `subcategory` (`SubCategoryID`),
+  ADD CONSTRAINT `item_ibfk_6` FOREIGN KEY (`PictureID`) REFERENCES `picture` (`PictureID`);
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`ItemID`) REFERENCES `item` (`ItemID`);
+
+--
+-- Constraints for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD CONSTRAINT `subcategory_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `category` (`CategoryID`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`ExpeditionID`) REFERENCES `expedition` (`ExpeditionID`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`RankID`) REFERENCES `rank` (`RankID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
